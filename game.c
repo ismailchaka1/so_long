70efc8a006c62228499286e67a54a7a1580eb4d5
ismailchaka1 <chakankar111@gmail.com>
2025-02-24 10:40:07 +0000

    Refactor texture loading and rendering functions for improved readability and maintainability

--- game.c
+++ game.c
@@ -6,152 +6,155 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 18:28:50 by root              #+#    #+#             */
-/*   Updated: 2025/02/03 02:38:02 by root             ###   ########.fr       */
+/*   Updated: 2025/02/18 19:58:42 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "so_long.h"
 
-
-void load_textures(t_game *game)
+void	load_textures(t_game *game)
 {
-    int img_width = TILE_SIZE;
-    int img_height = TILE_SIZE;
+	int	img_width;
+	int	img_height;
 
-    game->wall_img = mlx_xpm_file_to_image(game->mlx, "textures/wall.xpm", &img_width, &img_height);
-    game->empty_img = mlx_xpm_file_to_image(game->mlx, "textures/empty.xpm", &img_width, &img_height);
-    game->collectible_img = mlx_xpm_file_to_image(game->mlx, "textures/collectible.xpm", &img_width, &img_height);
-    game->exit_img = mlx_xpm_file_to_image(game->mlx, "textures/exit.xpm", &img_width, &img_height);
-    game->player_img = mlx_xpm_file_to_image(game->mlx, "textures/player.xpm", &img_width, &img_height);
-
-    if (!game->wall_img || !game->empty_img || !game->collectible_img || !game->exit_img || !game->player_img)
-        error_handle(game, "Failed to load textures");
+	img_width = TILE_SIZE;
+	img_height = TILE_SIZE;
+	game->wall_img = mlx_xpm_file_to_image(game->mlx, "textures/wall.xpm",
+			&img_width, &img_height);
+	game->empty_img = mlx_xpm_file_to_image(game->mlx, "textures/empty.xpm",
+			&img_width, &img_height);
+	game->collectible_img = mlx_xpm_file_to_image(game->mlx,
+			"textures/collectible.xpm", &img_width, &img_height);
+	game->exit_img = mlx_xpm_file_to_image(game->mlx, "textures/exit.xpm",
+			&img_width, &img_height);
+	game->player_img = mlx_xpm_file_to_image(game->mlx, "textures/player.xpm",
+			&img_width, &img_height);
+	if (!game->wall_img || !game->empty_img || !game->collectible_img
+		|| !game->exit_img || !game->player_img)
+		error_handle(game, "Failed to load textures");
 }
 
-
-int render_map(t_game *game)
+int	render_map(t_game *game)
 {
-    int x, y;
+	int	x;
+    int	y;
 
-    for (y = 0; y < game->map_height; y++)
-    {
-        for (x = 0; x < game->map_width; x++)
-        {
-            if (game->map[y][x] == WALL)
-                mlx_put_image_to_window(game->mlx, game->win, game->wall_img, x * TILE_SIZE, y * TILE_SIZE);
-            else if (game->map[y][x] == EMPTY)
-                mlx_put_image_to_window(game->mlx, game->win, game->empty_img, x * TILE_SIZE, y * TILE_SIZE);
-            else if (game->map[y][x] == COLLECTIBLE)
-                mlx_put_image_to_window(game->mlx, game->win, game->collectible_img, x * TILE_SIZE, y * TILE_SIZE);
-            else if (game->map[y][x] == EXIT)
-                mlx_put_image_to_window(game->mlx, game->win, game->exit_img, x * TILE_SIZE, y * TILE_SIZE);
-            else if (game->map[y][x] == PLAYER)
-                mlx_put_image_to_window(game->mlx, game->win, game->player_img, x * TILE_SIZE, y * TILE_SIZE);
-        }
-    }
-    return (0);
+	y = 0;
+	while (y < game->map_height)
+	{
+		x = 0;
+		while (x < game->map_width)
+		{
+			if (game->map[y][x] == WALL)
+				mlx_put_image_to_window(game->mlx, game->win, game->wall_img, x
+					* TILE_SIZE, y * TILE_SIZE);
+			else if (game->map[y][x] == EMPTY)
+				mlx_put_image_to_window(game->mlx, game->win, game->empty_img, x
+					* TILE_SIZE, y * TILE_SIZE);
+			else if (game->map[y][x] == COLLECTIBLE)
+				mlx_put_image_to_window(game->mlx, game->win,
+					game->collectible_img, x * TILE_SIZE, y * TILE_SIZE);
+			else if (game->map[y][x] == EXIT)
+				mlx_put_image_to_window(game->mlx, game->win, game->exit_img, x
+					* TILE_SIZE, y * TILE_SIZE);
+			else if (game->map[y][x] == PLAYER)
+				mlx_put_image_to_window(game->mlx, game->win, game->player_img,
+					x * TILE_SIZE, y * TILE_SIZE);
+			x++;
+		}
+		y++;
+	}
+	return (0);
 }
 
-void move_player(t_game *game, int new_x, int new_y)
+void	move_player(t_game *game, int new_x, int new_y)
 {
-    printf("new_x: %d, new_y: %d\n", new_x, new_y);
-    if (game->map[new_y][new_x] == WALL)
-        return;
-
-    if (game->map[new_y][new_x] == COLLECTIBLE)
-        game->collectibles--;
-
-    if (game->map[new_y][new_x] == EXIT && game->collectibles == 0)
-        printf("You win!\n");
-        // exit_with_success("You win!");
-
-    game->map[game->player_y][game->player_x] = EMPTY;
-    game->map[new_y][new_x] = PLAYER;
-    game->player_x = new_x;
-    game->player_y = new_y;
-    game->moves++;
-    printf("Moves: %d\n", game->moves);
-    render_map(game);
+	printf("new_x: %d, new_y: %d\n", new_x, new_y);
+	if (game->map[new_y][new_x] == WALL)
+		return ;
+	if (game->map[new_y][new_x] == COLLECTIBLE)
+		game->collectibles--;
+	if (game->map[new_y][new_x] == EXIT && game->collectibles == 0)
+		printf("You win!\n");
+	// exit_with_success("You win!");
+	game->map[game->player_y][game->player_x] = EMPTY;
+	game->map[new_y][new_x] = PLAYER;
+	game->player_x = new_x;
+	game->player_y = new_y;
+	game->moves++;
+	printf("Moves: %d\n", game->moves);
+	render_map(game);
 }
 
-int close_window(t_game *game)
+int	close_window(t_game *game)
 {
-    mlx_destroy_window(game->mlx, game->win);
-    exit(0);
+	mlx_destroy_window(game->mlx, game->win);
+	exit(0);
 }
 
-int key_press(int keycode, t_game *game)
+int	key_press(int keycode, t_game *game)
 {
-    if (keycode == 65307)
-        printf("ESC pressed\n");
-        // close_window(game);
-    else if (keycode == 119 || keycode == 65362)
-        move_player(game, game->player_x, game->player_y - 1);
-    else if (keycode == 65364 || keycode == 115)
-        move_player(game, game->player_x, game->player_y + 1);
-    else if (keycode == 65361 || keycode == 97)
-        move_player(game, game->player_x - 1, game->player_y);
-    else if (keycode == 65363 || keycode == 100)
-        move_player(game, game->player_x + 1, game->player_y);
-    return (0);
+	if (keycode == 65307)
+		printf("ESC pressed\n");
+	// close_window(game);
+	else if (keycode == 119 || keycode == 65362)
+		move_player(game, game->player_x, game->player_y - 1);
+	else if (keycode == 65364 || keycode == 115)
+		move_player(game, game->player_x, game->player_y + 1);
+	else if (keycode == 65361 || keycode == 97)
+		move_player(game, game->player_x - 1, game->player_y);
+	else if (keycode == 65363 || keycode == 100)
+		move_player(game, game->player_x + 1, game->player_y);
+	return (0);
 }
-void delay(int milliseconds)
+void	delay(int milliseconds)
 {
-    clock_t start_time = clock();
-    clock_t end_time = milliseconds * CLOCKS_PER_SEC / 1000 + start_time;
-    while (clock() < end_time)
-    {
-        // Busy-waiting
-    }
+	clock_t	start_time;
+	clock_t	end_time;
+
+	start_time = clock();
+	end_time = milliseconds * CLOCKS_PER_SEC / 1000 + start_time;
+	while (clock() < end_time)
+	{
+		// Busy-waiting
+	}
 }
 
-void zwa9_dzob(t_game *game)
+void	zwa9_dzob(t_game *game)
 {
-    int i = 0;
-    char *line;
-    while (1)
-    {
-        line = ft_strjoin("Loading ", ft_itoa(i));
-        mlx_clear_window(game->mlx, game->win);
-        mlx_string_put(game->mlx, game->win, 32, 32, 0xFFF, line);
-        i++;
-        free(line);
-        if (i == 50)
-            break;
-        delay(80);
-    }
-    delay(700);
-    mlx_clear_window(game->mlx, game->win);
-    mlx_string_put(game->mlx, game->win, 32, 32, 0xFFF, "Loading 100");
-    delay(500);
+	int		i;
+	char	*line;
+
+	i = 0;
+	while (1)
+	{
+		line = ft_strjoin("Loading ", ft_itoa(i));
+		mlx_clear_window(game->mlx, game->win);
+		mlx_string_put(game->mlx, game->win, 32, 32, 0xFFF, line);
+		i++;
+		free(line);
+		if (i == 50)
+			break ;
+		delay(80);
+	}
+	delay(700);
+	mlx_clear_window(game->mlx, game->win);
+	mlx_string_put(game->mlx, game->win, 32, 32, 0xFFF, "Loading 100");
+	delay(500);
 }
 
 void	game_init(t_game *game)
 {
-    game->mlx = mlx_init();
-    game->win = mlx_new_window(game->mlx, game->map_width * 32, game->map_height * 32, "so_long");
-    game->player_moves = 0;
-    game->moves = 0;
-    game->exit_reachable = 0;
-    load_textures(game);
-    zwa9_dzob(game);
-    render_map(game);
-    mlx_loop_hook(game->mlx, &render_map, game);
-    mlx_hook(game->win, 2, 1L << 0, &key_press, game);
-    mlx_loop(game->mlx);
+	game->mlx = mlx_init();
+	game->win = mlx_new_window(game->mlx, game->map_width * 32, game->map_height
+			* 32, "so_long");
+	game->player_moves = 0;
+	game->moves = 0;
+	game->exit_reachable = 0;
+	load_textures(game);
+	zwa9_dzob(game);
+	render_map(game);
+	mlx_loop_hook(game->mlx, &render_map, game);
+	mlx_hook(game->win, 2, 1L << 0, &key_press, game);
+	mlx_loop(game->mlx);
 }
-
-// void	game_init(t_game *game)
-// {
-//     game->mlx = mlx_init();
-//     game->win = mlx_new_window(game->mlx, game->map_width * 32, game->map_height * 32, "so_long");
-//     game->player_moves = 0;
-//     game->moves = 0;
-//     game->exit_reachable = 0;
-//     load_textures(game);
-//     render_map(game);
-//     mlx_loop_hook(game->mlx, &render_map, game);
-//     mlx_hook(game->win, 2, 1L << 0, &key_press, game);
-//     mlx_loop(game->mlx);
-// }
--- parsing.c
+++ parsing.c
@@ -6,140 +6,139 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 17:59:10 by root              #+#    #+#             */
-/*   Updated: 2025/02/01 18:41:08 by root             ###   ########.fr       */
+/*   Updated: 2025/02/18 20:14:00 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void error_handle(t_game *game, char *line)
+void	error_handle(t_game *game, char *line)
 {
-    int i;
+	int	i;
 
-    printf("Error: %s\n", line);
-
-    if (game->map)
-    {
-        i = 0;
-        while (game->map[i])
-        {
-            free(game->map[i]);
-            i++;
-        }
-        free(game->map);
-    }
-
-    exit(1);
+	printf("Error: %s\n", line);
+	if (game->map)
+	{
+		i = 0;
+		while (game->map[i])
+		{
+			free(game->map[i]);
+			i++;
+		}
+		free(game->map);
+	}
+	exit(1);
 }
 
-void free_mapcopy(char **map, int k)
+void	free_mapcopy(char **map)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (i < k)
-    {
-        free(map[i]);
-        i++;
-    }
-    free(map);
+	i = 0;
+	while (map[i])
+	{
+		free(map[i]);
+		i++;
+	}
+	free(map);
+	map = NULL;
 }
 
-void free_map(t_game *game, int k)
+void	free_map(t_game *game)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (i < k)
-    {
-        free(game->map[i]);
-        i++;
-    }
-    free(game->map);
+	i = 0;
+	while (i < game->map_height)
+	{
+		free(game->map[i]);
+		i++;
+	}
+	free(game->map);
+	game->map = NULL;
 }
 
-void flood_fill(char **map, int x, int y, int *collectibles, t_game *game)
+void	flood_fill(char **map, int x, int y, int *collectibles, t_game *game)
 {
-    // Base cases
-    if (x < 0 || y < 0 || x >= game->map_width || y >= game->map_height)
-        return; // Out of bounds
-    if (map[y][x] == WALL || map[y][x] == VISITED)
-        return; // Wall or already visited
+	char	current_tile;
 
-    // Mark the tile as visited
-    char current_tile =map[y][x];
-    map[y][x] = VISITED;
-
-    // Check if the tile is a collectible or exit
-    if (current_tile == COLLECTIBLE)
-        (*collectibles)--;
-    if (current_tile == EXIT)
-        game->exit_reachable = 1;
-
-    // Recursively explore neighboring tiles
-    flood_fill(map, x + 1, y, collectibles, game); // Right
-    flood_fill(map, x - 1, y, collectibles, game); // Left
-    flood_fill(map, x, y + 1, collectibles, game); // Down
-    flood_fill(map, x, y - 1, collectibles, game); // Up
+	// Base cases
+	if (x < 0 || y < 0 || x >= game->map_width || y >= game->map_height)
+		return ; // Out of bounds
+	if (map[y][x] == WALL || map[y][x] == VISITED)
+		return ; // Wall or already visited
+	// Mark the tile as visited
+	current_tile = map[y][x];
+	map[y][x] = VISITED;
+	// Check if the tile is a collectible or exit
+	if (current_tile == COLLECTIBLE)
+		(*collectibles)--;
+	if (current_tile == EXIT)
+		game->exit_reachable = 1;
+	// Recursively explore neighboring tiles
+	flood_fill(map, x + 1, y, collectibles, game); // Right
+	flood_fill(map, x - 1, y, collectibles, game); // Left
+	flood_fill(map, x, y + 1, collectibles, game); // Down
+	flood_fill(map, x, y - 1, collectibles, game); // Up
 }
-void validate_path(t_game *game)
+void	validate_path(t_game *game)
 {
-    char **map_copy;
-    int i;
-    int collectibles = game->collectibles;
-    map_copy = malloc(sizeof(char *) * (game->map_height + 1));
-    if (!map_copy)
-    {
-        perror("Malloc failed");
-        exit(1);
-    }
-    i = 0;
-    while (i < game->map_height)
-    {
-        map_copy[i] = ft_strdup(game->map[i]);
-        i++;
-    }
-    map_copy[i] = NULL;
-    // printf("map: %s\n", map_copy[1]);
-    flood_fill(map_copy, game->player_x, game->player_y, &collectibles, game);
-    free_mapcopy(map_copy, game->map_height);
-    if (collectibles != 0)
-        error_handle(game, "Collectibles are not reachable");
-    else if (game->exit_reachable != 1)
-        error_handle(game, "Exit is not reachable");
-    else   
-        game_init(game);
+	char	**map_copy;
+	int		i;
+	int		collectibles;
+
+	collectibles = game->collectibles;
+	map_copy = malloc(sizeof(char *) * (game->map_height + 1));
+	if (!map_copy)
+		exit(1);
+	i = 0;
+	while (i < game->map_height)
+	{
+		map_copy[i] = ft_strdup(game->map[i]);
+		i++;
+	}
+	map_copy[i] = NULL;
+	flood_fill(map_copy, game->player_x, game->player_y, &collectibles, game);
+	free_mapcopy(map_copy);
+	if (collectibles != 0)
+		error_handle(game, "Collectibles are not reachable");
+	else if (game->exit_reachable != 1)
+		error_handle(game, "Exit is not reachable");
+	else
+		game_init(game);
 }
 
-void validate_map(t_game *game)
+void	validate_map(t_game *game)
 {
-    int i;
-    int j;
-    i = 0;
-    while (i < game->map_height)
-    {
-        j = 0;
-        while (j < game->map_width)
-        {
-            if (i == 0 || i == game->map_height - 1 || j == 0 || j == game->map_width - 1)
-            {
-                if (game->map[i][j] != WALL)
-                    error_handle(game, "Map is not surrounded by walls");
-            }
-            if (!ft_strchr("01CEP", game->map[i][j]))
-                error_handle(game, "Invalid character in map");
+	int	i;
+	int	j;
 
-            j++;
-        }
-        i++;
-    }
-    validate_path(game);
+	i = 0;
+	while (i < game->map_height)
+	{
+		j = 0;
+		while (j < game->map_width)
+		{
+			if (i == 0 || i == game->map_height - 1 || j == 0
+				|| j == game->map_width - 1)
+			{
+				if (game->map[i][j] != WALL)
+					error_handle(game, "Map is not surrounded by walls");
+			}
+			if (!ft_strchr("01CEP", game->map[i][j]))
+				error_handle(game, "Invalid character in map");
+			j++;
+		}
+		i++;
+	}
+	validate_path(game);
 }
 
 void	check_map(t_game *game)
 {
 	int	i;
 	int	j;
+
 	i = 0;
 	while (i < game->map_height)
 	{
@@ -160,7 +159,7 @@ void	check_map(t_game *game)
 				game->collectibles++;
 			if (game->map[i][j] == EXIT)
 			{
-                game->exit_num++;
+				game->exit_num++;
 				game->exit_x = j;
 				game->exit_y = i;
 			}
@@ -171,82 +170,93 @@ void	check_map(t_game *game)
 	if (game->players_num != 1 || game->collectibles < 1 || game->exit_x == 0
 		|| game->exit_y == 0 || game->exit_num != 1)
 		error_handle(game, "Invalid map 2");
-    validate_map(game);
+	validate_map(game);
 }
 
-void readMap(char *map)
+int	count_map_height(int fd)
 {
-    t_game game;
-    int i;
-    int map_height;
-    int fd;
-    char *line;
+	int		map_height;
+	char	*line;
 
-    i = 0;
-    fd = open(map, O_RDONLY);
-    if (fd == -1)
-    {
-        perror("MAP NOT FOUND");
-        exit(1);
-    }
-    map_height = 0;
-    while (1)
-    {
-        line = get_next_line(fd);
-        if (!line)
-            break;
-        if (ft_strncmp(line, "\n", 2))
-            map_height++;
-        free(line);
-    }
-    close(fd);
+	map_height = 0;
+	while ((line = get_next_line(fd)))
+	{
+		if (ft_strncmp(line, "\n", 2))
+			map_height++;
+		free(line);
+	}
+	return (map_height);
+}
 
-    game.map_height = map_height;
-    game.map = malloc(sizeof(char *) * (map_height + 1));
-    if (!game.map)
-    {
-        perror("Malloc failed");
-        exit(1);
-    }
-    fd = open(map, O_RDONLY);
-    if (fd == -1)
-    {
-        perror("MAP NOT FOUND");
-        free(game.map);
-        exit(1);
-    }
+void	fixLine(t_game *game, int i)
+{
+	if (game->map[i][ft_strlen(game->map[i]) - 1] == '\n')
+		game->map[i][ft_strlen(game->map[i]) - 1] = 0;
+	if (ft_strlen(game->map[i]) == 0)
+		error_handle(game, "Empty line in map");
+}
 
-    i = 0;
-    game.map_width = 0;
-    game.map[game.map_height] = NULL;
-    while (1)
-    {
-        line = get_next_line(fd);
-        if (!line)
-            break;
-        game.map[i] = ft_strdup(line);
-        if (game.map[i][ft_strlen(game.map[i]) - 1] == '\n')
-            game.map[i][ft_strlen(game.map[i]) - 1] = 0;
-        free(line);
+void	read_map_data(int fd, t_game *game)
+{
+	int		i;
+	char	*line;
 
-        if (ft_strlen(game.map[i]) == 0)
-        {
-            error_handle(&game, "Empty line in map");
-        }
-        if (game.map_width == 0)
-        {
-            game.map_width = (int)ft_strlen(game.map[i]);
-        }
-        else if (ft_strlen(game.map[i]) != (size_t)game.map_width)
-        {
-           free_map(&game, i);
-           printf("Error: Map is not rectangular\n");
-           exit(1);
-        }
-        i++;
-    }
-    close(fd);
-    check_map(&game);
+	i = 0;
+	game->map_width = 0;
+	game->map[game->map_height] = NULL;
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			break ;
+		game->map[i] = ft_strdup(line);
+		fixLine(game, i);
+		free(line);
+		if (game->map_width == 0)
+			game->map_width = ft_strlen(game->map[i]);
+		else if (ft_strlen(game->map[i]) != (size_t)game->map_width)
+		{
+			free_map(game);
+			printf("Error: Map is not rectangular\n");
+			exit(1);
+		}
+		i++;
+	}
+	close(fd);
+}
+
+void	handle_file_error(int fd, char *message)
+{
+	if (fd == -1)
+	{
+		perror(message);
+		exit(1);
+	}
+}
+
+void	readMap(char *map)
+{
+	t_game	game;
+	int		fd;
+
+	fd = open(map, O_RDONLY);
+	if (fd == -1)
+		handle_file_error(fd, "MAP NOT FOUND");
+	game.map_height = count_map_height(fd);
+	game.map = malloc(sizeof(char *) * (game.map_height + 1));
+	if (!game.map)
+	{
+		perror("Malloc failed");
+		exit(1);
+	}
+	fd = open(map, O_RDONLY);
+	if (fd == -1)
+	{
+		free(game.map);
+		handle_file_error(fd, "MAP NOT FOUND");
+	}
+	read_map_data(fd, &game);
+	check_map(&game);
 }
 
 int	main(int ac, char **av)
